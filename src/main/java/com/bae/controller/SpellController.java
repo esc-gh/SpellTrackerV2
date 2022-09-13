c0dacc81da3e42bba3b3d6d87d3ce553750f02fd
esc-gh <94961352+esc-gh@users.noreply.github.com>
2022-09-13 09:56:05 +0100

    Updated Controller with new Service methods and Exceptions

--- src/main/java/com/bae/controller/SpellController.java
+++ src/main/java/com/bae/controller/SpellController.java
@@ -8,11 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,13 +54,13 @@ public class SpellController {
 		return spells;
 	}
 
-//	@GetMapping("/{name}")
-//	public ResponseEntity<Spell> getSpellByName(@PathVariable("name") String name) {
-//		Spell savedSpell = spellService.getByName(name);
-//
-//		ResponseEntity<Spell> response = ResponseEntity.status(HttpStatus.OK).body(savedSpell);
-//		return response;
-//	}
+	@GetMapping("/{name}")
+	public ResponseEntity<Spell> getSpellByName(@PathVariable("name") String name) {
+		Spell savedSpell = spellService.getByName(name);
+
+		ResponseEntity<Spell> response = ResponseEntity.status(HttpStatus.OK).body(savedSpell);
+		return response;
+	}
 
 	@GetMapping("/lv/{level}")
 	public ResponseEntity<List<Spell>> getSpellByLevel(@PathVariable("level") int level) {
@@ -88,36 +88,36 @@ public class SpellController {
 		return new ResponseEntity<Spell>(savedSpell, headers, HttpStatus.CREATED);
 	}
 
-//	@PutMapping("/{name}")
-//	public ResponseEntity<Spell> updateSpell(@PathVariable("name") String name, @Valid @RequestBody Spell spell) {
-//		Spell updatedSpell = spellService.updateByName(name, spell);
-//
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add("Location", "/spell/" + String.valueOf(updatedSpell.getName()));
-//
-//		return new ResponseEntity<Spell>(updatedSpell, headers, HttpStatus.ACCEPTED);
-//	}
-//	
-//	@PutMapping("/{id}")
-//	public ResponseEntity<Spell> updateSpell(@PathVariable("id") Long id, @Valid @RequestBody Spell spell) {
-//		Spell updatedSpell = spellService.updateById(id, spell);
-//
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add("Location", "/spell/" + String.valueOf(updatedSpell.getName()));
-//
-//		return new ResponseEntity<Spell>(updatedSpell, headers, HttpStatus.ACCEPTED);
-//	}
+	@PutMapping("/{name}")
+	public ResponseEntity<Spell> updateSpell(@PathVariable("name") String name, @Valid @RequestBody Spell spell) {
+		Spell updatedSpell = spellService.updateByName(name, spell);
 
-//	@DeleteMapping("/{name}")
-//	public ResponseEntity<?> deleteSpell(@PathVariable("name") String name) {
-//		spellService.deleteByName(name);
-//		return ResponseEntity.accepted().build();
-//	}
-//
-//	@DeleteMapping("/{id}")
-//	public ResponseEntity<?> deleteSpell(@PathVariable("id") Long id) {
-//		spellService.deleteById(id);
-//		return ResponseEntity.accepted().build();
-//	}
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/spell/" + String.valueOf(updatedSpell.getName()));
+
+		return new ResponseEntity<Spell>(updatedSpell, headers, HttpStatus.ACCEPTED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Spell> updateSpell(@PathVariable("id") Long id, @Valid @RequestBody Spell spell) {
+		Spell updatedSpell = spellService.updateById(id, spell);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/spell/" + String.valueOf(updatedSpell.getName()));
+
+		return new ResponseEntity<Spell>(updatedSpell, headers, HttpStatus.ACCEPTED);
+	}
+
+	@DeleteMapping("/{name}")
+	public ResponseEntity<?> deleteSpell(@PathVariable("name") String name) {
+		spellService.deleteByName(name);
+		return ResponseEntity.accepted().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteSpell(@PathVariable("id") Long id) {
+		spellService.deleteById(id);
+		return ResponseEntity.accepted().build();
+	}
 
 }
