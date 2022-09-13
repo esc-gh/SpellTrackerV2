25df67837ca7d66dff3b8c761ce3376a9f69bd08
esc-gh <94961352+esc-gh@users.noreply.github.com>
2022-09-13 09:21:50 +0100

    Spring starter project created

---
+++ src/main/java/com/bae/SpellTrackerV2Application.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellTrackerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellTrackerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/SpellTrackerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellTrackerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
