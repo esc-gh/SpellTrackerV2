84e7cb2ae19795e1f15267d5ca2e7ec93a3aaf3f
esc-gh <94961352+esc-gh@users.noreply.github.com>
2022-09-14 11:30:14 +0100

    Service tests WIP

---
+++ src/test/java/com/bae/service/SpellServiceIntegrationTest.java
@@ -0,0 +1,127 @@
+package com.bae.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.jdbc.Sql;
+
+import com.bae.entities.Spell;
+import com.bae.repos.SpellRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+//@Sql(scripts = { "classpath:testSchema.sql",
+//        "classpath:testData.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
+@Transactional
+public class SpellServiceIntegrationTest {
+
+	@Autowired
+	private SpellService spellService;
+
+	@Autowired
+	private SpellRepository spellRepository;
+
+	private List<Spell> spellsInDb;
+	private List<Spell> nameOrder;
+
+	@BeforeEach
+	public void init() {
+		List<Spell> spells = List.of(new Spell("Wish", 9, "Conjuration"), new Spell("Blade Ward", 0, "Abjuration"),
+				new Spell("Prismatic Wall", 9, "Abjuration"));
+		spellsInDb = new ArrayList<>();
+		spellsInDb.addAll(spellRepository.saveAll(spells));
+		nameOrder = spellsInDb.stream().sorted(Comparator.comparing(Spell::getName)).collect(Collectors.toList());
+	}
+
+	@Test
+	public void getAllSpellsTest() {
+		assertThat(nameOrder).isEqualTo(spellService.getAllByName());
+	}
+
+	@Test
+	public void getAllByNameTest() {
+		assertThat(nameOrder).isEqualTo(spellService.getAllByName());
+	}
+
+	@Test
+	public void getAllByLevelTest() {
+		List<Spell> levelOrder = nameOrder.stream().sorted(Comparator.comparing(Spell::getLevel))
+				.collect(Collectors.toList());
+		assertThat(levelOrder).isEqualTo(spellService.getAllByLevel());
+	}
+
+	@Test
+	public void getAllBySchoolTest() {
+		List<Spell> schoolOrder = nameOrder.stream().sorted(Comparator.comparing(Spell::getSchool))
+				.collect(Collectors.toList());
+		assertThat(schoolOrder).isEqualTo(spellService.getAllBySchool());
+	}
+
+	@Test
+	public void createSpellTest() {
+		Spell newSpell = new Spell("Scrying", 5, "Divination");
+		Spell expectedSpell = new Spell(newSpell.getName(), newSpell.getLevel(), newSpell.getSchool());
+
+		assertThat(expectedSpell).isEqualTo(spellService.create(newSpell));
+	}
+
+	@Test
+	public void getSpellByNameTest() {
+		Spell expectedSpell = new Spell("Prismatic Wall", 9, "Abjuration");
+		String spellName = "Prismatic Wall";
+
+		assertThat(expectedSpell).isEqualTo(spellService.getByName(spellName));
+	}
+
+	@Test
+	public void getSpellByLevelTest() {
+		List<Spell> expectedSpells = List.of(new Spell("Prismatic Wall", 9, "Abjuration"),
+				new Spell("Wish", 9, "Conjuration"));
+		int spellLevel = 9;
+
+		assertThat(expectedSpells).isEqualTo(spellService.getByLevel(spellLevel));
+	}
+
+	@Test
+	public void getSpellBySchoolTest() {
+		List<Spell> expectedSpells = List.of(new Spell("Blade Ward", 0, "Abjuration"),
+				new Spell("Prismatic Wall", 9, "Abjuration"));
+		String spellSchool = "Abjuration";
+
+		assertThat(expectedSpells).isEqualTo(spellService.getBySchool(spellSchool));
+	}
+
+	@Test
+	public void updateSpellTest() {
+		Spell updatedSpell = new Spell("Prismatic Wall", 7, "Necromancy");
+		Spell toUpdateWith = new Spell("Prismatic Wall", 7, "Necromancy");
+		String spellName = "Prismatic Wall";
+
+		assertThat(updatedSpell).isEqualTo(spellService.updateByName(spellName, toUpdateWith));
+	}
+
+	@Test
+	public void deleteSpellTest() {
+		Spell expectedSpell = spellsInDb.get(0);
+		String spellName = expectedSpell.getName();
+
+		spellService.deleteByName(spellName);
+
+		assertThat(spellRepository.findSpellByName(spellName)).isEqualTo(Optional.empty());
+	}
+
+}
---
+++ src/test/java/com/bae/service/SpellServiceUnitTest.java
@@ -0,0 +1,198 @@
+package com.bae.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.bae.entities.Spell;
+import com.bae.repos.SpellRepository;
+import com.bae.exceptions.SpellNotFoundException;
+
+@ExtendWith(MockitoExtension.class)
+public class SpellServiceUnitTest {
+
+	@Mock
+	private SpellRepository spellRepository;
+
+	@InjectMocks
+	private SpellService spellService;
+
+	private List<Spell> spells;
+	private Spell expectedSpell;
+
+	@BeforeEach
+	public void init() {
+		spells = List.of(new Spell("Wish", 9, "Conjuration"), new Spell("Blade Ward", 0, "Abjuration"),
+				new Spell("Prismatic Wall", 9, "Abjuration"));
+		expectedSpell = new Spell("Prismatic Wall", 9, "Abjuration");
+	}
+
+	@Test
+	public void getAllSpellsTest() {
+		when(spellRepository.findAll()).thenReturn(spells);
+		assertThat(spellService.getAll()).isEqualTo(spells);
+		verify(spellRepository).findAll();
+	}
+
+	@Test
+	public void getAllSpellsByNameTest() {
+		List<Spell> schoolOrder = spells.stream().sorted(Comparator.comparing(Spell::getName))
+				.collect(Collectors.toList());
+
+		when(spellRepository.findAll()).thenReturn(spells);
+		assertThat(spellService.getAllByName()).isEqualTo(schoolOrder);
+		verify(spellRepository).findAll();
+	}
+
+	@Test
+	public void getAllSpellsByNameLevel() {
+		List<Spell> nameOrder = spells.stream().sorted(Comparator.comparing(Spell::getName))
+				.collect(Collectors.toList());
+		List<Spell> levelOrder = nameOrder.stream().sorted(Comparator.comparing(Spell::getLevel))
+				.collect(Collectors.toList());
+
+		when(spellRepository.findAll()).thenReturn(spells);
+		assertThat(spellService.getAllByLevel()).isEqualTo(levelOrder);
+		verify(spellRepository).findAll();
+	}
+
+	@Test
+	public void getAllSpellsByNameSchool() {
+		List<Spell> schoolOrder = spells.stream().sorted(Comparator.comparing(Spell::getSchool))
+				.collect(Collectors.toList());
+
+		when(spellRepository.findAll()).thenReturn(spells);
+		assertThat(spellService.getAllBySchool()).isEqualTo(schoolOrder);
+		verify(spellRepository).findAll();
+	}
+
+	@Test
+	public void getByNameTest() {
+		String name = expectedSpell.getName();
+
+		when(spellRepository.findSpellByName(name)).thenReturn(Optional.of(expectedSpell));
+		assertThat(spellService.getByName(name)).isEqualTo(expectedSpell);
+		verify(spellRepository).findSpellByName(name);
+	}
+
+	@Test
+	public void getByNameInvalidTest() {
+		String name = "Afkjesgjskfj";
+
+		when(spellRepository.findSpellByName(name)).thenReturn(Optional.empty());
+
+		SpellNotFoundException snfe = Assertions.assertThrows(SpellNotFoundException.class, () -> {
+			spellService.getByName(name);
+		});
+
+		String expected = "Spell with name '" + name + "' cannot be found";
+		assertThat(snfe.getMessage()).isEqualTo(expected);
+
+		verify(spellRepository).findSpellByName(name);
+	}
+
+	@Test
+	public void getByLevelTest() {
+		List<Spell> levelOrder = spells.stream().sorted(Comparator.comparing(Spell::getName))
+				.collect(Collectors.toList());
+		int level = expectedSpell.getLevel();
+
+		when(spellRepository.findSpellByLevel(level)).thenReturn(levelOrder);
+		assertThat(spellService.getByLevel(level)).isEqualTo(levelOrder);
+		verify(spellRepository).findSpellByLevel(level);
+	}
+
+	@Test
+	public void getBySchoolTest() {
+		List<Spell> schoolOrder = spells.stream().sorted(Comparator.comparing(Spell::getSchool))
+				.collect(Collectors.toList());
+		String school = expectedSpell.getSchool();
+
+		when(spellRepository.findSpellBySchool(school)).thenReturn(schoolOrder);
+		assertThat(spellService.getBySchool(school)).isEqualTo(schoolOrder);
+		verify(spellRepository).findSpellBySchool(school);
+	}
+
+	@Test
+	public void createSpellTest() {
+		when(spellRepository.save(expectedSpell)).thenReturn(expectedSpell);
+		assertThat(spellService.create(expectedSpell)).isEqualTo(expectedSpell);
+		verify(spellRepository).save(expectedSpell);
+	}
+
+	@Test
+	public void updateSpellTest() {
+		String name = expectedSpell.getName();
+		Spell updatesToMake = new Spell(expectedSpell.getName(), expectedSpell.getLevel() + 1, expectedSpell.getSchool());
+		Spell updatedSpell = new Spell(expectedSpell.getId(), expectedSpell.getName(), expectedSpell.getLevel() + 1,
+				expectedSpell.getSchool());
+
+		when(spellRepository.existsByName(name)).thenReturn(true);
+		when(spellRepository.getByName(name)).thenReturn(expectedSpell);
+		when(spellRepository.save(expectedSpell)).thenReturn(updatedSpell);
+
+		assertThat(spellService.updateByName(name, updatesToMake)).isEqualTo(updatedSpell);
+
+		verify(spellRepository).existsByName(name);
+		verify(spellRepository).getByName(name);
+		verify(spellRepository).save(expectedSpell);
+	}
+
+	@Test
+	public void updateSpellInvalidTest() {
+		String name = "Afkjesgjskfj";
+
+		when(spellRepository.existsByName(name)).thenReturn(false);
+
+		SpellNotFoundException snfe = Assertions.assertThrows(SpellNotFoundException.class, () -> {
+			spellService.updateByName(name, expectedSpell);
+		});
+
+		String expected = "Spell with name '" + name + "' cannot be found";
+		assertThat(snfe.getMessage()).isEqualTo(expected);
+
+		verify(spellRepository).existsByName(name);
+	}
+
+	@Test
+	public void deleteSpellTest() {
+		String name = expectedSpell.getName();
+		when(spellRepository.existsByName(name)).thenReturn(true);
+
+		spellService.deleteByName(name);
+
+		assertThat(spellRepository.findSpellByName(name)).isEqualTo(Optional.empty());
+
+		verify(spellRepository).existsByName(name);
+		verify(spellRepository).deleteByName(name);
+	}
+
+	@Test
+	public void deleteSpellInvalidTest() {
+		String name = "Afkjesgjskfj";
+
+		when(spellRepository.existsByName(name)).thenReturn(false);
+
+		SpellNotFoundException snfe = Assertions.assertThrows(SpellNotFoundException.class, () -> {
+			spellService.deleteByName(name);
+		});
+
+		String expected = "Spell with name '" + name + "' cannot be found";
+		assertThat(snfe.getMessage()).isEqualTo(expected);
+
+		verify(spellRepository).existsByName(name);
+	}
+}
